71ae00fcc0be4ea9ea613cf02d5f9fcced10beaa
Josue Vasquez <108988576+ThisIsSkous@users.noreply.github.com>
2023-05-31 22:13:13 -0500

    Delete semana6_2_2_3_ejemplo_01-main directory

--- semana6_2_2_3_ejemplo_01-main/README.md
+++
@@ -1,2 +0,0 @@
-# semana6_2_2_3_ejemplo_01
-Acceso base de datos no relacionales desde lenguajes de programación
--- semana6_2_2_3_ejemplo_01-main/enlace_base.py
+++
@@ -1,6 +0,0 @@
-"""
-    Agregar información en una colección de MongoDB
-    desde Python
-"""
-from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27010/')
